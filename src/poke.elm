f6962895ac6f6dba043d3d9149117cc5383bfad2
Mini0n <mini0n.000@gmail.com>
2019-02-20 17:33:24 -0600

    Elm Init + Landing

---
+++ src/poke.elm
@@ -0,0 +1,14 @@
+module Poke exposing (main)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+view model =
+    div []
+        [ text "PokElm"
+        ]
+
+
+main =
+    view "no model yet"
